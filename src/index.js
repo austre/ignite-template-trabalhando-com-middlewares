2fd72267455bbd237e6e3154ef4c4677d5b6c41f
Neto-GAT <Neto-GAT@rn.sebrae.corp>
2022-04-27 17:11:42 -0300

    Desafio 02-Testes com falha

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 
-const { v4: uuidv4, validate } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
+const regexExp = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
 
 const app = express();
 app.use(express.json());
@@ -9,22 +10,78 @@ app.use(cors());
 
 const users = [];
 
+//Middlewares
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const user = users.find((user) => user.username === username)
+
+  if (!user) {
+    return response.status(404).json({ error: "Usuário não existe!" })
+  }
+
+  request.user = user
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  //const { username } = request.headers
+  const { user } = request.headers
+  //const user = users.find((user) => user.username === username)
+
+  if (!user.pro && user.todos.length > 9) {
+    return response.status(403).json({ error: "Limite de 10 todos atingido!" })
+  }
+
+  request.user = user
+  return next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+
+  //Validando o usuário
+  const user = users.find((user) => user.username === username)
+
+  if (!user) {
+    return response.status(404).json({ error: "Usuário não existe!" })
+  }
+
+  //Validando o uuid
+  const idValidation = regexExp.test(id)
+
+  if (!idValidation) {
+    return response.status(400).json({ error: "O ID é inválido!" })
+  }
+
+  //const todoIndex = user.todos.findIndex(todo => todo.id === id)
+  const todoIndex = user.todos.findIndex((todo) => todo.id === id)
+
+  if (todoIndex === -1) {
+    return response.status(404).json({ error: "O todo informado não foi encontrado!" })
+  }
+
+  const todo = user.todos.find((todo) => todo.id === id)
+
+  request.user = user
+  request.todo = todo
+  return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  const user = users.find((user) => user.id === id)
+
+  if (!user) {
+    return response.status(404).json({ error: "Usuário não existe!" })
+  }
+
+  request.user = user
+  return next()
 }
 
+//Rotas
 app.post('/users', (request, response) => {
   const { name, username } = request.body;
 
